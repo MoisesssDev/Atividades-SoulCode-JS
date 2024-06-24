e43589df009bce27b141face0e728292a308ebd5
Moises Almeida <moisesssprg@gmail.com>
2024-06-24 13:19:19 -0300

    melhora a resolução do exercicio 1

--- exercicio-1/index.js
+++ exercicio-1/index.js
@@ -9,12 +9,8 @@ console.log(`A temperatura em Celsius é: ${celsius.toFixed(2)}`);
 //  Crie cinco variáveis que representam cinco notas de um estudante. 
 //  Calcule a média ponderada, sabendo que o peso das notas são respectivamente: 3, 2, 1, 4 e 5.
 
-let nota1 = 10;
-let nota2 = 9;
-let nota3 = 8;
-let nota4 = 7;
-let nota5 = 6;
+notas = [10, 9, 8, 7, 6];
+pesos = [3, 2, 1, 4, 5];
 
-let media = (nota1 * 3 + nota2 * 2 + nota3 * 1 + nota4 * 4 + nota5 * 5) / 15;
-
+let media = (notas[0] * pesos[0] + notas[1] * pesos[1] + notas[2] * pesos[2] + notas[3] * pesos[3] + notas[4] * pesos[4]) / (pesos[0] + pesos[1] + pesos[2] + pesos[3] + pesos[4]);
 console.log(`A média ponderada é: ${media.toFixed(2)}`);
